db88f5fa27700e5a2b6f29f91ea55ebf4a4ae679
Joebert Jacaba <joebertj@gmail.com>
2018-02-23 18:01:43 +0800

    added substring mathes as well

--- word.py
+++ word.py
@@ -6,16 +6,36 @@ form = cgi.FieldStorage()
 drow = form.getvalue("word").lower()
 lendrow=len(drow)
 ldrow=list(drow)
+ldrow.sort()
 words=open("/home/kenchlig/words","r")
 print("<ul>")
 for word in words:
-   if(lendrow==len(word)-1):
+   lenword=len(word)-1
+   if(lenword>2 and lendrow>=lenword):
+      word=word.lower()
       lword=list(word)
       lword=lword[:-1]
-      if(set(ldrow)==set(lword)):
+      lword.sort()
+      i = 0
+      j = 0
+      anomaly = 0
+      while (i<lenword and j<lendrow):
+            if(ldrow[j]!=lword[i]):
+               anomaly = 1
+               j+=1
+            else:
+               anomaly = 0
+               i+=1
+               j+=1
+               if(j == lendrow and i < lenword):
+                   anomaly = 1
+      if(anomaly == 0):
          print("<li>")
+         #print(i,j,ldrow, lword)
          print(word.upper())
          print("</li>")
+      #if(anomaly == 1):
+         #print(i,j,ldrow, lword)
 words.close()
 print("</ul>")
 
