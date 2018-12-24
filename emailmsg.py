77609e2b5c4dc10c0864549c2e8f83ad7d8998eb
Joebert Jacaba <joebertj@gmail.com>
2018-12-23 20:56:59 -0500

    added rsvp and email scripts

--- emailmsg.py
+++ emailmsg.py
@@ -10,8 +10,14 @@ email = form.getvalue("email")
 subject = form.getvalue("subject")
 message = form.getvalue("message")
 mail = [ 'mail', '-s', subject, '-r', email, '--', 'joebert@kenchlightyear.com']
-body = [ 'echo', 'Message from: ' + fname + ' ' + lname + ' ' + message ]
+body = [ 'printf', 'Message from: ' + fname + ' ' + lname + '\n\n' + message ]
 bodyproc = subprocess.Popen(body, stdout=subprocess.PIPE)
 mailproc = subprocess.Popen(mail, stdin=bodyproc.stdout)
-print("Thank you for contacting us. We have received your message. We will get back to you soon.")
+confirmation = "Thank you for contacting us. We have received your message. We will get back to you soon."
+signature = "Regards,\nJoebert Jacaba\nCEO, Kench Lightyear\n+63 919 999 2056"
+mail = [ 'mail', '-s', "Confirmation", '-r', 'joebert@kenchlightyear.com', '--', email ]
+body = [ 'printf', 'Hello ' + fname + ',\n\n' + confirmation + '\n\n' + signature]
+bodyproc = subprocess.Popen(body, stdout=subprocess.PIPE)
+mailproc = subprocess.Popen(mail, stdin=bodyproc.stdout)
+print(confirmation)
 print('<p><a href="javascript:self.close()">Close</a></p>')
